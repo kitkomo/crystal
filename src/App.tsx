b300e27bac590fce5631412da7711e2900f56654
quive <unjusodal@gmail.com>
2023-04-06 19:52:32 +0400

    favicon & smooth cart appearing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+import { useAppSelector } from './store/hooks'
 
 import Header from './components/Header/Header'
 import Cart from './components/Cart/Cart'
@@ -7,15 +9,17 @@ import ShopPage from './pages/ShopPage'
 import Footer from './components/Footer/Footer'
 import ContactPage from './pages/ContactPage'
 import AboutPage from './pages/AboutPage'
-import FavoritesPage from './pages/FavoritesPage';
-import ProductPage from './pages/ProductPage';
+import FavoritesPage from './pages/FavoritesPage'
+import ProductPage from './pages/ProductPage'
 
 const App: React.FC = () => {
-
+	const { open } = useAppSelector(state => state.cart)
 	return (
 		<div className='app-wrapper'>
 			<Header />
-			<Cart />
+			<CSSTransition in={open} timeout={150} unmountOnExit classNames='cart'>
+				<Cart />
+			</CSSTransition>
 			<main className='main'>
 				<Routes>
 					<Route path='/' element={<ShopPage />} />
